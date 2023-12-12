bbeb4d2866e69f66874a64f2ae3f2ef9ef7918cf
Rick South <rick@natamus.com>
2023-12-12 10:55:22 +0100

    Better Beacon Placement source code for 1.20.3.

---
+++ NeoForge/src/main/java/com/natamus/betterbeaconplacement/neoforge/events/NeoForgeBeaconEvent.java
@@ -0,0 +1,29 @@
+package com.natamus.betterbeaconplacement.neoforge.events;
+
+import com.natamus.betterbeaconplacement.events.BeaconEvent;
+import com.natamus.collective.functions.WorldFunctions;
+import net.minecraft.world.level.Level;
+import net.neoforged.neoforge.event.entity.player.PlayerInteractEvent;
+import net.neoforged.neoforge.event.level.BlockEvent;
+import net.neoforged.bus.api.SubscribeEvent;
+import net.neoforged.fml.common.Mod.EventBusSubscriber;
+
+@EventBusSubscriber
+public class NeoForgeBeaconEvent {
+	@SubscribeEvent
+	public static void onBeaconClick(PlayerInteractEvent.RightClickBlock e) {
+		if (!BeaconEvent.onBeaconClick(e.getLevel(), e.getEntity(), e.getHand(), e.getPos(), e.getHitVec())) {
+			e.setCanceled(true);
+		}
+	}
+	
+	@SubscribeEvent
+	public static void onBlockBreak(BlockEvent.BreakEvent e) {
+		Level level = WorldFunctions.getWorldIfInstanceOfAndNotRemote(e.getLevel());
+		if (level == null) {
+			return;
+		}
+
+		BeaconEvent.onBlockBreak(level, e.getPlayer(), e.getPos(), e.getState(), null);
+	}
+}
