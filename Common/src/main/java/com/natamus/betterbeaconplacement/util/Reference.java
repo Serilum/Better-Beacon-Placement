cd0f81246e6176a94667b0b91418f9f7401ef29b
Rick South <rick@natamus.com>
2023-12-18 10:40:58 +0100

    (v3.3) Improved mod icon compression, which lowers the jar file size. [+2]

--- Common/src/main/java/com/natamus/betterbeaconplacement/util/Reference.java
+++ Common/src/main/java/com/natamus/betterbeaconplacement/util/Reference.java
@@ -3,6 +3,6 @@ package com.natamus.betterbeaconplacement.util;
 public class Reference {
 	public static final String MOD_ID = "betterbeaconplacement";
 	public static final String NAME = "Better Beacon Placement";
-	public static final String VERSION = "3.2";
+	public static final String VERSION = "3.3";
 	public static final String ACCEPTED_VERSIONS = "[1.19.2]";
 }
